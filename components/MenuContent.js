fe90de1766ba01640bcda2b45333a9ba614a2eaa
instaer <33076883+instaer@users.noreply.github.com>
2023-08-24 11:50:16 +0800

    Use rehype-raw to parse html tags; cancel the automatic line wrap of each line of code (replace it with scrolling)

--- components/MenuContent.js
+++ components/MenuContent.js
@@ -3,12 +3,14 @@ import remarkGfm from 'remark-gfm';
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 import {materialOceanic as style} from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import nextConfig from 'next.config';
+import rehypeRaw from "rehype-raw";
 
 export default function MenuContent({content}) {
     return (
         <ReactMarkdown
             className='markdown-body'
             remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeRaw]}
             transformImageUri={(path) => {
                 return `${nextConfig.basePath || ''}${path}`;
             }}
@@ -20,9 +22,8 @@ export default function MenuContent({content}) {
                             {...props}
                             style={style}
                             language={match[1]}
-                            PreTag="div"
                             showLineNumbers
-                            wrapLines
+                            wrapLines={false}
                             lineProps={{style: {wordBreak: 'break-all', whiteSpace: 'pre-wrap'}}}
                         >
                             {String(children).replace(/\n$/, '')}
